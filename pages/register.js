9310a14c7421b00e5b0bd3ceb5d3c629cb42e65a
Jose Bustillo <josebustillodealba@gmail.com>
2020-10-05 21:34:07 -0500

    feat: register

--- pages/register.js
+++ pages/register.js
@@ -4,7 +4,64 @@ import Layout from '../components/Layout';
 const Register = () => {
     return (
         <Layout>
-            <h1>Register</h1>
+            <h1 className="text-center text-2xl text-white font-light">Register</h1>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-sm">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                placeholder="Name"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+                                Last name
+                            </label>
+                            <input
+                                type="text"
+                                id="lastName"
+                                placeholder="Last name"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Email address"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+
+                        <input
+                            type="submit"
+                            className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"
+                            value="Register"
+                        />
+                    </form>
+                </div>
+            </div>
         </Layout>
     );
 }
