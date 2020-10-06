78630ad02baf947281ae7cce3798caa02bfd5d38
Jose Bustillo <josebustillodealba@gmail.com>
2020-10-05 21:28:00 -0500

    feat: login

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,41 @@ const Login = () => {
     return (
         <Layout>
             <h1 className="text-center text-2xl text-white font-light">Login</h1>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-sm">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Email address"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+
+                        <input
+                            type="submit"
+                            className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"
+                            value="Log In"
+                        />
+                    </form>
+                </div>
+            </div>
         </Layout>
     );
 }
