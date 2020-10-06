50a9b224b4713130bbefa7df9edff774400d7363
Jose Bustillo <josebustillodealba@gmail.com>
2020-10-05 22:16:33 -0500

    feat: add formik to the register form

--- pages/register.js
+++ pages/register.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Register = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            lastName: '',
+            email: '',
+            password: '',
+        },
+        onSubmit: values => {
+            console.log(values);
+        }
+    });
+
     return (
         <Layout>
             <h1 className="text-center text-2xl text-white font-light">Register</h1>
 
             <div className="flex justify-center mt-5">
                 <div className="w-full max-w-sm">
-                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                    <form
+                        className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                        onSubmit={formik.handleSubmit}
+                    >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                                 Name
@@ -17,6 +34,8 @@ const Register = () => {
                                 type="text"
                                 id="name"
                                 placeholder="Name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
@@ -28,6 +47,8 @@ const Register = () => {
                                 type="text"
                                 id="lastName"
                                 placeholder="Last name"
+                                value={formik.values.lastName}
+                                onChange={formik.handleChange}
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
@@ -39,6 +60,8 @@ const Register = () => {
                                 type="email"
                                 id="email"
                                 placeholder="Email address"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
@@ -50,6 +73,8 @@ const Register = () => {
                                 type="password"
                                 id="password"
                                 placeholder="Password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
