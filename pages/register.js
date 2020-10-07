fe28eb761c4158a8f83c58cfebda5e6f74adcf74
Jose Bustillo <josebustillodealba@gmail.com>
2020-10-06 20:45:00 -0500

    feat: validations with yup

--- pages/register.js
+++ pages/register.js
@@ -11,6 +11,12 @@ const Register = () => {
             email: '',
             password: '',
         },
+        validationSchema: Yup.object({
+            name: Yup.string().required('Name is required'),
+            lastName: Yup.string().required('Lastname is required'),
+            email: Yup.string().email('Email is not valid').required('Email is required'),
+            password: Yup.string().required("Password can't be empty").min(6, 'The password must be at least 6 characters'),
+        }),
         onSubmit: values => {
             console.log(values);
         }
@@ -36,9 +42,18 @@ const Register = () => {
                                 placeholder="Name"
                                 value={formik.values.name}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
+
+                        { formik.touched.name && formik.errors.name && (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.name}</p>
+                            </div>
+                        )}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
                                 Last name
@@ -52,6 +67,14 @@ const Register = () => {
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
+
+                        { formik.touched.lastName && formik.errors.lastName && (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.lastName}</p>
+                            </div>
+                        )}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                                 Email
@@ -65,6 +88,14 @@ const Register = () => {
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
+
+                        { formik.touched.email && formik.errors.email && (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.email}</p>
+                            </div>
+                        )}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                 Password
@@ -78,6 +109,13 @@ const Register = () => {
                                 className="shadow appereance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             />
                         </div>
+
+                        { formik.touched.password && formik.errors.password && (
+                            <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                <p className="font-bold">Error</p>
+                                <p>{formik.errors.password}</p>
+                            </div>
+                        )}
 
                         <input
                             type="submit"
